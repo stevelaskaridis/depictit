26f38b9fdfdf36ea35719eb1684944bc9d1f0886
Steve Laskaridis <stevelaskaridis@gmail.com>
2016-09-17 21:00:00 +0200

    Log incoming message

--- depictit_backend/depictit_backend/views.py
+++ depictit_backend/depictit_backend/views.py
@@ -1,7 +1,7 @@
 import os
-# import sys
-# import json
-# import requests
+import sys
+import json
+import requests
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from models import *
@@ -21,6 +21,7 @@ def verify(request):
 @api_view(['POST'])
 # @app.route('/', methods=['POST'])
 def webhook(request):
+    log(request)
     return Response({"message": "OK!"})
 
 #     # endpoint for processing incoming messaging events
@@ -61,33 +62,32 @@ def webhook(request):
 #     return "ok", 200
 #
 #
-# def send_message(recipient_id, message_text):
-#     log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
-#
-#     params = {
-#         "access_token": os.environ["PAGE_ACCESS_TOKEN"]
-#     }
-#     headers = {
-#         "Content-Type": "application/json"
-#     }
-#     data = json.dumps({
-#         "recipient": {
-#             "id": recipient_id
-#         },
-#         "message": {
-#             "text": message_text
-#         }
-#     })
-#     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers,
-#                       data=data)
-#     if r.status_code != 200:
-#         log(r.status_code)
-#         log(r.text)
-#
-#
-# def log(message):  # simple wrapper for logging to stdout on heroku
-#     print str(message)
-#     sys.stdout.flush()
+def send_message(recipient_id, message_text):
+    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers,
+                      data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
+
+def log(message):  # simple wrapper for logging to stdout on heroku
+    print str(message)
+    sys.stdout.flush()
 #
 #
 # if __name__ == '__main__':
